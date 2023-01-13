be52f7c6cd965de1d9144ae36ae3b1d9e21ef6f8
Leah Roberson <77908412+leah-crypto@users.noreply.github.com>
2023-01-13 12:05:17 -0600

    initial

--- src/Square.jsx
+++ src/Square.jsx
@@ -4,27 +4,27 @@ import "./App.css";
 const Square = (props) => {
   // console.log(<props className="squares"></props>, props.player);
   const handleClick = () => {
-    if(!props.squareValue){
-      if(props.player){
+    if (!props.squareValue) {
+      if (props.player) {
         props.squares.splice(props.index, 1, "X");
         props.setSquares(props.squares);
         props.setPlayer(!props.player);
-      }else{
+      } else {
         props.squares.splice(props.index, 1, "O");
         props.setSquares(props.squares);
         props.setPlayer(!props.player);
       }
     }
-      
-
   };
-          return(
-            <div className="square" onClick={handleClick}>
-              {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
-            </div>
-          );
-  
+  return (
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === "O" ? (
+        <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" />
+      ) : (
+        props.squareValue
+      )}
+    </div>
+  );
 };
 
 export default Square;
-
